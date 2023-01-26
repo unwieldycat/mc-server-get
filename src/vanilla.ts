4c401e5b83875a9fb3c5a0bef1843de0bd03a2d7
Thurston Yates <thurstonalexyates@gmail.com>
2023-01-26 09:00:51 -0500

    Add code to get vanilla download

---
+++ src/vanilla.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { program } from './main';
+
+// ============================= Main function ============================= //
+
+interface VersionMeta {
+	id: string;
+	type: string;
+	url: string;
+	time: string;
+	releaseTime: string;
+	sha1: string;
+	complianceLevel: number;
+}
+
+interface Manifest {
+	latest: { release: string; snapshot: string };
+	versions: VersionMeta[];
+}
+
+async function main(version: string): Promise<void> {
+	if (version) {
+		const versionRegexp = /(\d{2}w\d{2}\w+)|(1\.\d{1,2}(\.\d{1,2})?(-(pre|rc)\d{1})?)/;
+		const versionIsValid = versionRegexp.test(version);
+		if (!versionIsValid) program.error('Invalid version string!');
+	}
+
+	const manifest: Manifest = await (async () => {
+		const res = await axios.get('https://piston-meta.mojang.com/mc/game/version_manifest_v2.json');
+		if (res.status !== 200) program.error('An error occurred getting the version manifest');
+		return res.data;
+	})();
+
+	if (!version) version = manifest.latest.release;
+
+	const versionManifest = await (async () => {
+		const meta = manifest.versions.find((v) => v.id === version);
+		if (!meta) program.error(`Unable to find version ${version}`);
+
+		const res = await axios.get(meta.url);
+		return res.data;
+	})();
+
+	// TODO: Download file
+}
+
+export default main;
